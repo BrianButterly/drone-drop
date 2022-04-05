81ed913903ea52238c4b916774f2594cf6aa3382
Brian Butterly <butterlybrian@gmail.com>
2022-04-05 16:32:24 -0500

    starting point

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Menu from "./components/Menu";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <>
+        <Menu />
+      </>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,21 @@
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+function Menu() {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">
+          <img src="logo.png" alt="drone logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {/* <Nav.Link href="/home">Home</Nav.Link> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Menu;
