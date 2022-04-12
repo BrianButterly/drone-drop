533bc37d8f046772461025ba9c97958c7813f78f
Brian Butterly <butterlybrian@gmail.com>
2022-04-11 20:14:09 -0500

    Updates

---
+++ precache-manifest.3924a6973c5522fb08b65c3e0d4ab1b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "208d72e49b2e475f97a7",
+    "url": "/drone-drop/static/css/main.e0fd1b9c.chunk.css"
+  },
+  {
+    "revision": "208d72e49b2e475f97a7",
+    "url": "/drone-drop/static/js/main.208d72e4.chunk.js"
+  },
+  {
+    "revision": "424a0a03ef8d9a5c794a",
+    "url": "/drone-drop/static/css/1.7f111f8b.chunk.css"
+  },
+  {
+    "revision": "424a0a03ef8d9a5c794a",
+    "url": "/drone-drop/static/js/1.424a0a03.chunk.js"
+  },
+  {
+    "revision": "a2d5c0eedf2bed2a1d84",
+    "url": "/drone-drop/static/js/2.a2d5c0ee.chunk.js"
+  },
+  {
+    "revision": "7c8ef44e5cc74c7fb85c",
+    "url": "/drone-drop/static/js/runtime~main.7c8ef44e.js"
+  },
+  {
+    "revision": "68774263ad8fda67ed7953babacf23bc",
+    "url": "/drone-drop/index.html"
+  }
+];
